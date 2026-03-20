e0f7420a89a33aac48adb4097d02be2497c5f75f
monu242 <monucut657@gmail.com>
2026-03-20 14:02:03 +0530

    add. fetched the data from user(linkedin)

---
+++ PLATFORMS/linkedin.py
@@ -0,0 +1,6 @@
+def fetch_linkedin_profile(user_username,bio,aim):
+    #function to fetch linkedin profile data from user input and return it as dectionary.
+    profiles = {"username":user_username,
+               "bio":bio,
+               "aim":aim}
+    return profiles 
