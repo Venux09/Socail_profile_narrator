d24c7fdf8863775632e904701fb6f49b06b51b43
monu242 <monucut657@gmail.com>
2026-03-20 14:01:12 +0530

    add . fetched the data of the user

---
+++ PLATFORMS/instagram.py
@@ -0,0 +1,6 @@
+def fetch_instagram_profile(user_username,bio):
+    #function to fetch instagram profile data from user input and return it as dictionary.
+    profiles = {"username":user_username,
+            "bio":bio,
+            "platform":"instagram"}
+    return profiles 
